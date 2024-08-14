1292377539b4b8833f2120656e998eb7283c6d7e
Dennis Herbrik <dennisherbrik1988@gmail.com>
2024-08-14 12:02:06 +0200

    Updated toml file

--- src/chat_ui/init_flask_app.py
+++ src/chat_ui/init_flask_app.py
@@ -6,7 +6,7 @@ DESIRED_ROOT = pathlib.Path(__file__).parent
 app = Flask(__name__)
 app.secret_key = "140918"
 # Use this if you start without docker-compose
-# redis_client = redis.StrictRedis(host="localhost", port=6379, db=0)
+redis_client = redis.StrictRedis(host="localhost", port=6379, db=0)
 
 # Use this if you start with docker-compose
-redis_client = redis.StrictRedis(host="redis", port=6379, db=0)
+# redis_client = redis.StrictRedis(host="redis", port=6379, db=0)
